3acb1c0bc36378a097e5ce8139c5e99af58a5423
Ilias <cepctapk@gmail.com>
2023-02-06 21:05:59 +0300

    Initial Commit

---
+++ SlicerColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlicerColor
+//
+//  Created by Commander on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
